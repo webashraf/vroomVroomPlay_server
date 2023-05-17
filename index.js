51cccca4170b5b02c050f182837eb18b95c118db
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-17 23:06:58 +0600

    conect server side to privae repo

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//------------Middel were -----------//
+//------------Middel were -----------//
+app.use(cors());
+app.use(express.json());
+//-------------------------------------//
+//------------------------------------//
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>TOY MARKETPLACE IS RUNING</h1>");
+  });
+  
+  app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+  });
